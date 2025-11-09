13560afb27c3e219370f090b6a760d03fce154e4
TilekarPranav <pranavtilekar8034@gmail.com>
2025-11-09 11:53:35 +0530

    CORES UPDATE

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://exammate-hupf.onrender.com",
+  "https://exammates.onrender.com",
   "https://exammate-admins.onrender.com",
 ];
 
