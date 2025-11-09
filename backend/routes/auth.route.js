6a71c4fee393f1509e351be1a7c11e33ff491269
TilekarPranav <pranavtilekar8034@gmail.com>
2025-11-09 12:19:55 +0530

    AUTH CHANGES

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -14,14 +14,12 @@ import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get("/", verifyToken, getAllUsers);
-
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
-
 router.get("/check-auth", verifyToken, checkAuth);
 
 export default router;
