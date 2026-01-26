676c9a91af56d5e41ebb3c6dd7dabef8576bd406
TilekarPranav <pranavtilekar8034@gmail.com>
2026-01-26 10:44:07 +0530

    Refactor quiz routes: remove image upload middleware and streamline route definitions

--- backend/routes/quiz.route.js
+++ backend/routes/quiz.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { validateObjectId } from "../middleware/validateObjectId.js";
-import upload from "../middleware/upload.js";
 import {
   Create,
   Update,
@@ -16,21 +15,16 @@ import {
 
 const router = express.Router();
 
-// Public access
-router.get("/result", verifyToken, getUserResults);
 router.get("/display", Display);
 router.get("/display/:id", DisplayByIdForUser);
 
-// Protected routes
-router.post("/create", verifyToken, upload.single("image"), Create);
-router.put("/update/:id", verifyToken, upload.single("image"), Update);
+router.get("/result", verifyToken, getUserResults);
+router.post("/submit/:id", verifyToken, validateObjectId("id"), SubmitQuiz);
+
+router.post("/create", verifyToken, Create);
+router.put("/update/:id", verifyToken, validateObjectId("id"), Update);
 router.delete("/delete/:id", verifyToken, validateObjectId("id"), Delete);
 router.get("/all", verifyToken, getAllQuizzes);
 router.get("/:id", verifyToken, validateObjectId("id"), getQuizById);
-
-// Submit quiz
-router.post("/submit/:id", verifyToken, validateObjectId("id"), SubmitQuiz);
-
-// Get all results of logged-in user
 
 export default router;
